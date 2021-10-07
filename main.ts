39e6cd88ce4bf4682f1f26988f1a9096f8938aa0
Adéla Ditrichová <91247571+adeladitrichova@users.noreply.github.com>
2021-10-07 11:32:30 +0200

    allowed_01

--- main.ts
+++ main.ts
@@ -1,1 +1,13 @@
-
+let povoleno = false
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    povoleno = true
+})
+basic.forever(function on_forever() {
+    if (povoleno) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+    
+})
